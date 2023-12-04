db3c52b84191f36d28d4492ed097beb86be14c59
Franck Schmidlin <franck.schmidlin@theverygroup.com>
2023-12-04 06:22:05 +0000

    day 4 part 1 - raw

--- day04/Day04.test.ts
+++ day04/Day04.test.ts
@@ -1,20 +1,44 @@
 import { describe, expect, test } from '@jest/globals';
-import { Day04 } from './Day04'
+import { Day04, doubleNTimes, parseCard, findWinningNumbers } from './Day04'
 
 const day = new Day04();
 test("base path points to __dirname", () => expect(day.basePath()).toBe(__dirname));
 
 //////// PART 1
 
+test('doubleNTimes', () => {
+  expect(doubleNTimes(0)).toBe(0);
+  expect(doubleNTimes(1)).toBe(1);
+  expect(doubleNTimes(2)).toBe(2);
+  expect(doubleNTimes(3)).toBe(4);
+  expect(doubleNTimes(4)).toBe(8);
+});
+
+test('doubleNTimes', () => {
+  expect(parseCard('Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53')).toStrictEqual({
+    winningNumbers: new Set(['41', '48', '83', '86', '17']),
+    numbers: ['83', '86', '6', '31', '17', '9', '48', '53']
+  }
+  );
+});
+
+test('findWinningNumbers', () => {
+  expect(findWinningNumbers(
+    ['83', '86', '6', '31', '17', '9', '48', '53'], 
+    new Set(['41', '48', '83', '86', '17']))).toStrictEqual(['41','48','83','86',17])
+  }
+);
+
+
 test('example 1', () => {
-    expect(day.part1Example()).toBe("???");
-    return day.readAndReturnEntries(day.testFilePart1()).then(entries => expect(day.part1(entries)).toBe(day.part1Example()));
-  });
+  expect(day.part1Example()).toBe("13");
+  return day.readAndReturnEntries(day.testFilePart1()).then(entries => expect(day.part1(entries)).toBe(day.part1Example()));
+});
 
 
 //////// PART 2
 
 test('example 2', () => {
-    expect(day.part2Example()).toBe("???");
-    return day.readAndReturnEntries(day.testFilePart2()).then(entries => expect(day.part2(entries)).toBe(day.part2Example()));
-  });
+  expect(day.part2Example()).toBe("???");
+  return day.readAndReturnEntries(day.testFilePart2()).then(entries => expect(day.part2(entries)).toBe(day.part2Example()));
+});
--- day04/Day04.ts
+++ day04/Day04.ts
@@ -4,16 +4,20 @@ export class Day04 extends Day {
     testFilePart1(){ return 'test.txt'};
     testFilePart2(){ return 'test.txt'};
     part1Example(): string {
-        return "???"
+        return "13"
     }
     part2Example(): string {
         return "???"
     }
 
     part1(entries : string[]): string {
-        let solution = "???"
+        let solution = 0
         for (let i = 0; i < entries.length; i++) {
             console.log(`entry = ${entries[i]}`);
+            let entry = entries[i];
+            const { numbers, winningNumbers } = parseCard(entry);
+            const W = findWinningNumbers(numbers, winningNumbers);
+            solution += doubleNTimes(W);
         }
         return `${solution}`;
     };
@@ -32,4 +36,23 @@ export class Day04 extends Day {
         return __dirname;
     };
 
+}
+
+export function findWinningNumbers(numbers: string[], winningNumbers: Set<string>) {
+    const myWinningNumbers = numbers.filter((n) => winningNumbers.has(n));
+    const W = myWinningNumbers.length;
+    return W;
+}
+
+export function parseCard(entry: string) {
+    let matches = entry.match(/Card\s+(?<card>\d+): (?<winningNumbers>.*) \| (?<numbers>.*)/);
+    if (!matches || !matches.groups) throw `Cant parse ${entry}`;
+    const card = matches.groups.card;
+    const winningNumbers = new Set(matches.groups.winningNumbers.split(/\s+/));
+    const numbers = matches.groups.numbers.split(/\s+/);
+    return { numbers, winningNumbers };
+}
+
+export function doubleNTimes(W: number) {
+    return (W > 1) ? 2 ** (W - 1) : (W > 0) ? 1 : 0;
 }
