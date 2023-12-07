d53a0fef1dcdcde7309987ebdac0678a8c43647e
Franck Schmidlin <franck.schmidlin@theverygroup.com>
2023-12-07 14:38:00 +0000

    Day 7 - part 2 - raw

--- day07/Day07.test.ts
+++ day07/Day07.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { Day07, calculateHandWithoutJokers, parseHand, compareHands, sortHandsInAscendingRankingOrder} from './Day07'
+import { Day07, calculateHandWithoutJokers, parseHand, compareHands, sortHandsInAscendingRankingOrder, symbolStrengthPart1, symbolStrengthPart2, calculateHandWithJokers} from './Day07'
 
 const day = new Day07();
 test("base path points to __dirname", () => expect(day.basePath()).toBe(__dirname));
@@ -23,7 +23,7 @@ const typesOfHands = [
 ]
 
 
-const testHands = [
+const testHandsPart1 = [
   { symbols: '32T3K'.split(''), type: 1, bid: 765 },
   { symbols: 'T55J5'.split(''), type: 3, bid: 684 },
   { symbols: 'KK677'.split(''), type: 2, bid: 28 },
@@ -31,15 +31,15 @@ const testHands = [
   { symbols: 'QQQJA'.split(''), type: 3, bid: 483 }
 ]
 //////// PART 1
-test('parseHand(entry: string) : Hand', () => {
-  expect(parseHand(testEntries[0], calculateHandWithoutJokers)).toStrictEqual(testHands[0]);
-  expect(parseHand(testEntries[1], calculateHandWithoutJokers)).toStrictEqual(testHands[1]);
-  expect(parseHand(testEntries[2], calculateHandWithoutJokers)).toStrictEqual(testHands[2]);
-  expect(parseHand(testEntries[3], calculateHandWithoutJokers)).toStrictEqual(testHands[3]);
-  expect(parseHand(testEntries[4], calculateHandWithoutJokers)).toStrictEqual(testHands[4]);
+test('part 1 - parseHand(entry: string) : Hand', () => {
+  expect(parseHand(testEntries[0], calculateHandWithoutJokers)).toStrictEqual(testHandsPart1[0]);
+  expect(parseHand(testEntries[1], calculateHandWithoutJokers)).toStrictEqual(testHandsPart1[1]);
+  expect(parseHand(testEntries[2], calculateHandWithoutJokers)).toStrictEqual(testHandsPart1[2]);
+  expect(parseHand(testEntries[3], calculateHandWithoutJokers)).toStrictEqual(testHandsPart1[3]);
+  expect(parseHand(testEntries[4], calculateHandWithoutJokers)).toStrictEqual(testHandsPart1[4]);
 });
 
-test('calculateHand(h: Hand) : number', () => {
+test('calculateHandWithoutJokers(h: Hand) : number', () => {
   expect(calculateHandWithoutJokers(typesOfHands[0].split(''))).toBe(6);  // Five of a kind
   expect(calculateHandWithoutJokers(typesOfHands[1].split(''))).toBe(5);  // Four of a kind
   expect(calculateHandWithoutJokers(typesOfHands[2].split(''))).toBe(4);  // Full house
@@ -48,31 +48,31 @@ test('calculateHand(h: Hand) : number', () => {
   expect(calculateHandWithoutJokers(typesOfHands[5].split(''))).toBe(1);  // One pair
   expect(calculateHandWithoutJokers(typesOfHands[6].split(''))).toBe(0);  // High Card
 
-  expect(calculateHandWithoutJokers(testHands[0].symbols)).toBe(1);  // 1 pair
-  expect(calculateHandWithoutJokers(testHands[1].symbols)).toBe(3);  // 3 of a kind
-  expect(calculateHandWithoutJokers(testHands[2].symbols)).toBe(2);  // 2 pairs
-  expect(calculateHandWithoutJokers(testHands[3].symbols)).toBe(2);  // 2 pairs
-  expect(calculateHandWithoutJokers(testHands[4].symbols)).toBe(3);  // 3 of a kind
+  expect(calculateHandWithoutJokers(testHandsPart1[0].symbols)).toBe(1);  // 1 pair
+  expect(calculateHandWithoutJokers(testHandsPart1[1].symbols)).toBe(3);  // 3 of a kind
+  expect(calculateHandWithoutJokers(testHandsPart1[2].symbols)).toBe(2);  // 2 pairs
+  expect(calculateHandWithoutJokers(testHandsPart1[3].symbols)).toBe(2);  // 2 pairs
+  expect(calculateHandWithoutJokers(testHandsPart1[4].symbols)).toBe(3);  // 3 of a kind
 });
 
-test('compareHands(a: Hand, b: Hand): number', () =>{
-  expect(compareHands(testHands[0],testHands[0])).toBe(0);
-  expect(compareHands(testHands[0],testHands[1])).toBeGreaterThan(0);  // first hand smaller than all others
-  expect(compareHands(testHands[0],testHands[2])).toBeGreaterThan(0);  // first hand smaller than all others
-  expect(compareHands(testHands[0],testHands[3])).toBeGreaterThan(0);  // first hand smaller than all others
-  expect(compareHands(testHands[0],testHands[4])).toBeGreaterThan(0);  // first hand smaller than all others
+test('part 1 - compareHands(a: Hand, b: Hand, , symbolStrengthPart): number', () =>{
+  expect(compareHands(testHandsPart1[0],testHandsPart1[0], symbolStrengthPart1)).toBe(0);
+  expect(compareHands(testHandsPart1[0],testHandsPart1[1], symbolStrengthPart1)).toBeGreaterThan(0);  // first hand smaller than all others
+  expect(compareHands(testHandsPart1[0],testHandsPart1[2], symbolStrengthPart1)).toBeGreaterThan(0);  // first hand smaller than all others
+  expect(compareHands(testHandsPart1[0],testHandsPart1[3], symbolStrengthPart1)).toBeGreaterThan(0);  // first hand smaller than all others
+  expect(compareHands(testHandsPart1[0],testHandsPart1[4], symbolStrengthPart1)).toBeGreaterThan(0);  // first hand smaller than all others
 
-  expect(compareHands(testHands[4],testHands[4])).toBe(0);
-  expect(compareHands(testHands[4],testHands[0])).toBeLessThan(0);     // last hand higher than all others
-  expect(compareHands(testHands[4],testHands[1])).toBeLessThan(0);     // last hand higher than all others
-  expect(compareHands(testHands[4],testHands[2])).toBeLessThan(0);     // last hand higher than all others
-  expect(compareHands(testHands[4],testHands[3])).toBeLessThan(0);     // last hand higher than all others
+  expect(compareHands(testHandsPart1[4],testHandsPart1[4], symbolStrengthPart1)).toBe(0);
+  expect(compareHands(testHandsPart1[4],testHandsPart1[0], symbolStrengthPart1)).toBeLessThan(0);     // last hand higher than all others
+  expect(compareHands(testHandsPart1[4],testHandsPart1[1], symbolStrengthPart1)).toBeLessThan(0);     // last hand higher than all others
+  expect(compareHands(testHandsPart1[4],testHandsPart1[2], symbolStrengthPart1)).toBeLessThan(0);     // last hand higher than all others
+  expect(compareHands(testHandsPart1[4],testHandsPart1[3], symbolStrengthPart1)).toBeLessThan(0);     // last hand higher than all others
 });
 
-test('sortHandsInAscendingRankingOrder(hands: Hand[])', () =>{
-  const handsBeforeSorting = testHands.map(h=>h);
-  sortHandsInAscendingRankingOrder(testHands);
-  expect(testHands).toStrictEqual([
+test('part 1 - sortHandsInAscendingRankingOrder(hands: Hand[])', () =>{
+  const handsBeforeSorting = testHandsPart1.map(h=>h);
+  sortHandsInAscendingRankingOrder(testHandsPart1, symbolStrengthPart1);
+  expect(testHandsPart1).toStrictEqual([
     handsBeforeSorting[0],
     handsBeforeSorting[3],
     handsBeforeSorting[2],
@@ -81,7 +81,6 @@ test('sortHandsInAscendingRankingOrder(hands: Hand[])', () =>{
   ]);
 });
 
-
 test('example 1', () => {
   expect(day.part1Example()).toBe("6440");
   expect(day.part1(testEntries)).toBe("6440");
@@ -89,8 +88,51 @@ test('example 1', () => {
 
 
 //////// PART 2
+const testHandsPart2 = [
+  { symbols: '32T3K'.split(''), type: 1, bid: 765 },
+  { symbols: 'T55J5'.split(''), type: 5, bid: 684 },
+  { symbols: 'KK677'.split(''), type: 2, bid: 28 },
+  { symbols: 'KTJJT'.split(''), type: 5, bid: 220 },
+  { symbols: 'QQQJA'.split(''), type: 5, bid: 483 }
+]
+
+test('part 2 - parseHand(entry: string) : Hand', () => {
+  expect(parseHand(testEntries[0], calculateHandWithJokers)).toStrictEqual(testHandsPart2[0]);
+  expect(parseHand(testEntries[1], calculateHandWithJokers)).toStrictEqual(testHandsPart2[1]);
+  expect(parseHand(testEntries[2], calculateHandWithJokers)).toStrictEqual(testHandsPart2[2]);
+  expect(parseHand(testEntries[3], calculateHandWithJokers)).toStrictEqual(testHandsPart2[3]);
+  expect(parseHand(testEntries[4], calculateHandWithJokers)).toStrictEqual(testHandsPart2[4]);
+});
+
+test('part 2 - calculateHandWithJokers(h: Hand) : number', () => {
+  expect(calculateHandWithJokers(typesOfHands[0].split(''))).toBe(6);  // Five of a kind
+  expect(calculateHandWithJokers(typesOfHands[1].split(''))).toBe(5);  // Four of a kind
+  expect(calculateHandWithJokers(typesOfHands[2].split(''))).toBe(4);  // Full house
+  expect(calculateHandWithJokers(typesOfHands[3].split(''))).toBe(3);  // Three of a kind
+  expect(calculateHandWithJokers(typesOfHands[4].split(''))).toBe(2);  // Two pairs
+  expect(calculateHandWithJokers(typesOfHands[5].split(''))).toBe(1);  // One pair
+  expect(calculateHandWithJokers(typesOfHands[6].split(''))).toBe(0);  // High Card
+
+  expect(calculateHandWithJokers(testHandsPart2[0].symbols)).toBe(1);  // 1 pair
+  expect(calculateHandWithJokers(testHandsPart2[1].symbols)).toBe(5);  // 4 of a kind
+  expect(calculateHandWithJokers(testHandsPart2[2].symbols)).toBe(2);  // 2 pairs
+  expect(calculateHandWithJokers(testHandsPart2[3].symbols)).toBe(5);  // 4 of a kind
+  expect(calculateHandWithJokers(testHandsPart2[4].symbols)).toBe(5);  // 4 of a kind
+});
+
+test('part 2 - sortHandsInAscendingRankingOrder(hands: Hand[])', () =>{
+  const handsBeforeSorting = testHandsPart2.map(h=>h);
+  sortHandsInAscendingRankingOrder(testHandsPart2, symbolStrengthPart2);
+  expect(testHandsPart2).toStrictEqual([
+    handsBeforeSorting[0],
+    handsBeforeSorting[2],
+    handsBeforeSorting[1],
+    handsBeforeSorting[4],
+    handsBeforeSorting[3],
+  ]);
+});
 
 test('example 2', () => {
-  expect(day.part2Example()).toBe("???");
+  expect(day.part2Example()).toBe("5905");
   return day.readAndReturnEntries(day.testFilePart2()).then(entries => expect(day.part2(entries)).toBe(day.part2Example()));
 });
--- day07/Day07.ts
+++ day07/Day07.ts
@@ -1,7 +1,12 @@
 import { Day } from "../lib/Day";
 
-const symbolStrength = new Map<string, number>();
-['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'].reverse().forEach((s, i) => symbolStrength.set(s, i));
+const symbolsListPart1 = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+export const symbolStrengthPart1 = new Map<string, number>();
+symbolsListPart1.reverse().forEach((s, i) => symbolStrengthPart1.set(s, i));
+
+const symbolsListPart2 = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'];
+export const symbolStrengthPart2 = new Map<string, number>();
+symbolsListPart2.reverse().forEach((s, i) => symbolStrengthPart2.set(s, i));
 
 interface Hand {
     symbols: string[];
@@ -9,12 +14,12 @@ interface Hand {
     bid: number;
 }
 
-export function parseHand(entry: string, calculateHand : (symbols: string[]) => number): Hand {
+export function parseHand(entry: string, calculateHand: (symbols: string[]) => number): Hand {
     const parts = entry.split(' ');
     const symbols = parts[0].split('');
     return {
         symbols,
-        type: calculateHandWithoutJokers(symbols),
+        type: calculateHand(symbols),
         bid: parseInt(parts[1])
     };
 }
@@ -33,20 +38,69 @@ export function calculateHandWithoutJokers(symbols: string[]): number {
     }
 }
 
+// return 0 for high card to 6 for 5 of a kind
+export function calculateHandWithJokers(symbols: string[]): number {
+    const symbolsCount = new Map<string, { symbol: string, count: number }>();
+
+    symbols.forEach(symbol => {
+        symbolsCount.set(symbol, symbolsCount.has(symbol) ? { symbol, count: symbolsCount.get(symbol)!.count + 1 } : { symbol, count: 1 });
+    });
+
+    // By Decreasing Count Then Strength (unless a joker)
+    let symbolsRanked = Array.from(symbolsCount.entries()).sort((a, b) => {
+        let symbolA = symbolsCount.get(a[1].symbol)!;
+        let symbolB = symbolsCount.get(b[1].symbol)!;
+        if (symbolA.symbol == 'J') return 1; // everything higher than J regardless of count
+        if (symbolB.symbol == 'J') return -1;  // everything higher than J regardless of count
+        if (symbolA.count == symbolB.count) return symbolStrengthPart2.get(symbolB.symbol)! - symbolStrengthPart2.get(symbolA.symbol)!;
+        return symbolB.count - symbolA.count;
+    });
+
+    let jokers = symbolsCount.has('J') ? symbolsCount.get('J')!.count : 0;
+
+    // remove the joker entries and distribute the jokers
+    symbolsRanked = symbolsRanked.filter(s=> s[0]  != 'J');
+    symbolsRanked.forEach(s => {
+        while (s[1].count < 5 && jokers > 0){
+             s[1].count++;
+             jokers--;
+        }
+    });
+    if (jokers == 5) return 6; // 5 remaining jokers can be turned into 5 of a kind 
+
+    switch (symbolsRanked.length) {
+        case 1: return 6;                                               // 5 of a kind (5 identical symbols (inc jokers))
+        case 2: {                               // 2 sets of symbols
+            switch (symbolsRanked[0][1].count) {
+                case 4: return 5;                                       // 4 of a kind (4+ 1)
+                default: return 4;                                      // full house (3+2)
+            }
+        }
+        case 3: {                              // 3 sets of symbols
+            switch (symbolsRanked[0][1].count) {
+                case 3: return 3;                                       // 3 of a kind (3+1+1)
+                default: return 2 ;                                     // 2 pairs (2+2+1)
+            }
+        }
+        case 4: return  1;                                              // 1 pair (2+1+1+1)
+        default: return 0;                                              // high hand
+    }
+}
+
 // If the result is negative, a is sorted before b, A is stronger than B
 // If the result is positive, b is sorted before a, B is stronger than A
-export function compareHands(a: Hand, b: Hand): number {
+export function compareHands(a: Hand, b: Hand, symbolStrength: Map<string, number>): number {
     // Hands are primarily ordered based on type; for example, every full house is stronger than any three of a kind.
     if (a.type != b.type) return b.type - a.type; // if b stronger than a, return positive number
     for (let s = 0; s < a.symbols.length; s++) {
-        if(a.symbols[s] == b.symbols[s]) continue; // If the nth card in each hand have the same label, move on to considering the next card in each hand. 
+        if (a.symbols[s] == b.symbols[s]) continue; // If the nth card in each hand have the same label, move on to considering the next card in each hand. 
         return symbolStrength.get(b.symbols[s])! - symbolStrength.get(a.symbols[s])!; // the hand with the stronger nth card is considered stronger (positive result)
     }
     return 0; // cards are identical
 }
 
-export function sortHandsInAscendingRankingOrder(hands: Hand[]) {
-    hands.sort((a, b) => compareHands(b, a));
+export function sortHandsInAscendingRankingOrder(hands: Hand[], symbolStrength: Map<string, number>) {
+    hands.sort((a, b) => compareHands(b, a, symbolStrength));
 }
 export class Day07 extends Day {
     testFilePart1() { return 'test.txt' };
@@ -55,7 +109,7 @@ export class Day07 extends Day {
         return "6440"
     }
     part2Example(): string {
-        return "???"
+        return "5905"
     }
 
     part1(entries: string[]): string {
@@ -67,16 +121,22 @@ export class Day07 extends Day {
             const symbols = entry[0].split('');
             hands.push(parseHand(entries[i], calculateHandWithoutJokers));
         }
-        sortHandsInAscendingRankingOrder(hands);
-        hands.forEach((h, i) => solution += (i+1) * h.bid);
+        sortHandsInAscendingRankingOrder(hands, symbolStrengthPart1);
+        hands.forEach((h, i) => solution += (i + 1) * h.bid);
         return `${solution}`;
     };
 
     part2(entries: string[]): string {
-        let solution = "???"
+        let solution = 0
+        const hands = [] as Hand[];
         for (let i = 0; i < entries.length; i++) {
             console.log(`entry = ${entries[i]}`);
+            const entry = entries[i].split(' ');
+            const symbols = entry[0].split('');
+            hands.push(parseHand(entries[i], calculateHandWithJokers));
         }
+        sortHandsInAscendingRankingOrder(hands, symbolStrengthPart2);
+        hands.forEach((h, i) => solution += (i + 1) * h.bid);
         return `${solution}`;
     };
 
