399b24264a4379a7d24922e417e05b15c1e41008
Franck Schmidlin <franck.schmidlin@theverygroup.com>
2023-12-11 10:09:58 +0000

    Day 11 - part 2 - very raw - runs like a hog

--- day11/Day11.ts
+++ day11/Day11.ts
@@ -58,11 +58,11 @@ function part1Implementation(entries: string[]) {
     }));
 
     console.log(`Found ${galaxyPairs.length} pairs of galaxies:`);
-    const galaxyPairMinimumDistances = galaxyPairs.map(p => Math.abs(p.g2.x - p.g1.x)+Math.abs(p.g2.y - p.g1.y));
+    const galaxyPairMinimumDistances = galaxyPairs.map(p => Math.abs(p.g2.x - p.g1.x) + Math.abs(p.g2.y - p.g1.y));
 
-    //galaxyPairNames.forEach((p,i) => console.log(`${('0' + i).slice(-4)} : ${p} : ${galaxyPairMinimumDistances[i]}`))
+    galaxyPairNames.forEach((p,i) => console.log(`${('0' + i).slice(-4)} : ${p} : ${galaxyPairMinimumDistances[i]}`))
     console.log(`Distances : ${galaxyPairMinimumDistances}`);
-    let solution = galaxyPairMinimumDistances.reduce((a,b)=> a+b);
+    let solution = galaxyPairMinimumDistances.reduce((a, b) => a + b);
     console.log(`${galaxyPairs.length} pairs add to ${solution}`)
     return `${solution}`;
 }
@@ -71,6 +71,56 @@ function part1Implementation(entries: string[]) {
 // ACTUAL CODE - Part TWO  //
 /////////////////////////////
 function part2Implementation(entries: string[]) {
-    let solution = '???'
+    const universe = Grid.fromEntries<string>(entries, (s) => s);
+
+    universe.logToConsole(c => c!);
+
+    // locate expansion zones
+    let emptyCols = [] as number[];
+    let emptyRows = [] as number[];
+    for (let x = 0; x<=universe.maxX; x++) {
+        if (!universe.getCol(x).includes('#')) emptyCols.push(x);
+    }
+    for (let y = 0; y <=universe.maxY; y++) {
+        if (!universe.getRow(y).includes('#')) emptyRows.push(y);
+    }
+    console.log(`empty rows : ${emptyRows}, empty cols : ${emptyCols}, galaxy after expansion =`);
+
+    //emptyCols.forEach(x => universe.insertColumnAt(x, '.'));
+    //emptyRows.forEach(y => universe.insertRowAt(y, '.'));
+    //universe.logToConsole(c => c!);
+
+    // get the galaxy coordinates
+    const E = 1000000-1;
+    const galaxies = [] as { x: number, y: number }[]
+    console.log(`Galaxies:`);
+    for (let X = 0; X <= universe.maxX; X++) {
+        for (let Y = 0; Y <= universe.maxY; Y++) {
+            if (universe.cells[X][Y] == '#') {
+                const expansionOffestX = emptyCols.filter(x => x < X).length * E;
+                const expansionOffestY = emptyRows.filter(y =>y < Y ).length  * E;
+                galaxies.push({ x: X + expansionOffestX, y: Y + expansionOffestY });
+                //console.log(`(${X},${Y}) => (${X} + ${expansionOffestX},${Y} + ${expansionOffestY}) => (${X+expansionOffestX},${Y+expansionOffestY})`);
+            }
+        }
+    }
+
+    const galaxyPairs = [] as { g1: { x: number, y: number }, g2: { x: number, y: number } }[];
+    const galaxyPairNames = [] as string[];
+   
+    galaxies.forEach(g1 => galaxies.forEach(g2 => {
+        if ((g2 != g1) && !(galaxyPairNames.includes(`(${g2.x},${g2.y})(${g1.x},${g1.y})`))) {
+            galaxyPairs.push({ g1, g2 });
+            galaxyPairNames.push(`(${g1.x},${g1.y})(${g2.x},${g2.y})`);
+        }
+    }));
+
+    console.log(`Found ${galaxyPairs.length} pairs of galaxies:`);
+    const galaxyPairMinimumDistances = galaxyPairs.map(p => Math.abs(p.g2.x - p.g1.x) + Math.abs(p.g2.y - p.g1.y));
+
+    galaxyPairNames.forEach((p,i) => console.log(`${('0' + i).slice(-4)} : ${p} : ${galaxyPairMinimumDistances[i]}`))
+    //console.log(`Distances : ${galaxyPairMinimumDistances}`);
+    let solution = galaxyPairMinimumDistances.reduce((a, b) => a + b);
+    console.log(`${galaxyPairs.length} pairs add to ${solution}`)
     return `${solution}`;
 }
