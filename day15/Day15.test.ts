7a0ab9c02e8885c89c7cb5b9b66241e6b1a30ae3
Franck Schmidlin <franck.schmidlin@theverygroup.com>
2023-12-15 15:36:58 +0000

    Day 15 part 2 - very raw

--- day15/Day15.test.ts
+++ day15/Day15.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals';
 import { determineDataFileName, readEntriesFromDataFile } from '../lib/AdventOfCodeChallenge';
-import { hash, part1, part2} from './Day15'
+import { Box, applyInstruction, hash, part1, part2} from './Day15'
 
 
 //////// PART 1
@@ -21,8 +21,35 @@ test('hash()', () => {
 });
 
 //////// PART 2
+const boxes = [] as Box[];
+for(let b=0; b<256; b++) boxes.push(new Map<string, { position: number; focal: string; }>());
+
+test('hash()', () => {
+  expect(hash('rn')).toBe('0');
+  expect(hash('cm')).toBe('0');
+  expect(hash('qp')).toBe('1');
+});
+
 test('example 2', () => {
-  expect(part2.example).toBe("???");
-  const dataFilePath = determineDataFileName(part2, true);
-  return readEntriesFromDataFile(dataFilePath).then(entries => expect(part2(entries)).toBe(part2.example));
+  applyInstruction('rn=1', boxes);
+  expect(boxes[0].size).toBe(1);
+  expect(boxes[0].get('rn')).toStrictEqual({position:0, focal:'1'});
+
+  applyInstruction('cm-', boxes);
+  expect(boxes[0].size).toBe(1);
+  expect(boxes[0].get('rn')).toStrictEqual({position:0, focal:'1'});
+
+  applyInstruction('qp=3', boxes);
+  expect(boxes[0].size).toBe(1);
+  expect(boxes[0].get('rn')).toStrictEqual({position:0, focal:'1'});
+  expect(boxes[1].size).toBe(1);
+  expect(boxes[1].get('qp')).toStrictEqual({position:0, focal:'3'});
+
+  applyInstruction('cm=2', boxes);
+  expect(boxes[0].size).toBe(2);
+  expect(boxes[0].get('rn')).toStrictEqual({position:0, focal:'1'});
+  expect(boxes[0].get('cm')).toStrictEqual({position:1, focal:'2'});
+  expect(boxes[1].size).toBe(1);
+  expect(boxes[1].get('qp')).toStrictEqual({position:0, focal:'3'});
+
 });
--- day15/Day15.ts
+++ day15/Day15.ts
@@ -17,12 +17,12 @@ part2.inputFile = 'input.txt';
 /////////////////////////////
 // ACTUAL CODE - Part ONE  //
 /////////////////////////////
-export function hash(string : string) : string{
-    return string.split('').reduce((previous, current, index) => `${((parseInt(previous) + current.charCodeAt(0))*17)%256}`, "0" );
+export function hash(string: string): string {
+    return string.split('').reduce((previous, current, index) => `${((parseInt(previous) + current.charCodeAt(0)) * 17) % 256}`, "0");
 }
 function part1Implementation(entries: string[]) {
     let solution = 0;
-    entries.flatMap(entry => entry.split(',')).forEach(e => solution+= parseInt(hash(e)));
+    entries.flatMap(entry => entry.split(',')).forEach(e => solution += parseInt(hash(e)));
     return `${solution}`;
 }
 
@@ -30,6 +30,58 @@ function part1Implementation(entries: string[]) {
 // ACTUAL CODE - Part TWO  //
 /////////////////////////////
 function part2Implementation(entries: string[]) {
-    let solution = '???'
-    return `${solution}`;
+    const boxes = [] as Box[];
+    for (let b = 0; b < 256; b++) boxes.push(new Map<string, { position: number; focal: string; }>());
+    entries.flatMap(entry => entry.split(',')).forEach((e, b) => {
+        applyInstruction(e, boxes);
+    });
+
+    let focussingPower = 0;
+    boxes.forEach((box, index) => {
+
+        Array.from(box.entries()).forEach(A => {
+            const label = A[0];
+            const b = A[1];
+            const lensePower = (index +1) * (b.position +1) * parseInt(b.focal)
+            console.log(`${label} : ${(index +1)}(Box ${index}) * ${b.position +1} (slot ${b.position}) * ${b.focal} (focal length) = ${lensePower}`);
+            focussingPower += lensePower
+        });
+    });
+
+    return `${focussingPower}`;
+}
+
+export type Box = Map<string, { position: number; focal: string; }>;
+export function applyInstruction(e: string, boxes: Box[]): Box {
+    const m = e.match(/(?<label>.*)(?<operation>[=|\-])(?<focal>\d)?/);
+    const label = m!.groups!.label;
+    const operation = m!.groups!.operation;
+    const focal = operation === '=' ? m!.groups!.focal : '';
+    const boxId = parseInt(hash(label))
+    const box = boxes[boxId];
+    if (operation === '=') {
+        if (box.has(label)) {
+            // If there is already a lens in the box with the same label, 
+            // replace the old lens with the new lens: remove the old lens 
+            // and put the new lens in its place, not moving any other lenses in the box.
+            box.set(label, { position: box.get(label)!.position, focal });
+        } else {
+            // If there is not already a lens in the box with the same label, 
+            // add the lens to the box immediately behind any lenses already in the box. 
+            // Don't move any of the other lenses when you do this. If there aren't any lenses in the box, 
+            // the new lens goes all the way to the front of the box.
+            box.set(label, { position: box.size, focal });
+        }
+    } else {
+        if(box.has(label)){
+            const emptiedPosition = box.get(label)!.position;
+            box.delete(label);
+            Array.from(box.keys()).forEach(k => {
+                if(box.get(k)!.position > emptiedPosition){
+                    box.set(k, {position : box.get(k)!.position-1, focal : box.get(k)!.focal});
+                }
+            });
+        }
+    }
+    return box;
 }
