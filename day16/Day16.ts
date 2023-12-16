983b952fc640af236756007335ffea252f05d6e3
Franck Schmidlin <franck.schmidlin@theverygroup.com>
2023-12-16 09:31:12 +0000

    day 16 - part 2 - very inneficient

--- day16/Day16.ts
+++ day16/Day16.ts
@@ -184,31 +184,216 @@ function part1Implementation(entries: string[]) {
     
     return `${energised}`;
 
-    function goUp(currentLocation: { x: number; y: number; direction: number; }) {
-        currentLocation.y--;
-        currentLocation.direction = 3; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
-    }
-
-    function goLeft(currentLocation: { x: number; y: number; direction: number; }) {
-        currentLocation.x--;
-        currentLocation.direction = 2; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
-    }
-
-    function goDown(currentLocation: { x: number; y: number; direction: number; }) {
-        currentLocation.y++;
-        currentLocation.direction = 1; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
-    }
-
-    function goRight(currentLocation: { x: number; y: number; direction: number; }) {
-        currentLocation.x++;
-        currentLocation.direction = 0; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
-    }
 }
 
+function goUp(currentLocation: { x: number; y: number; direction: number; }) {
+    currentLocation.y--;
+    currentLocation.direction = 3; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+}
+
+function goLeft(currentLocation: { x: number; y: number; direction: number; }) {
+    currentLocation.x--;
+    currentLocation.direction = 2; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+}
+
+function goDown(currentLocation: { x: number; y: number; direction: number; }) {
+    currentLocation.y++;
+    currentLocation.direction = 1; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+}
+
+function goRight(currentLocation: { x: number; y: number; direction: number; }) {
+    currentLocation.x++;
+    currentLocation.direction = 0; // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+}
 /////////////////////////////
 // ACTUAL CODE - Part TWO  //
 /////////////////////////////
 function part2Implementation(entries: string[]) {
-    let solution = '???'
-    return `${solution}`;
+    const grid = Grid.fromEntries(entries, c => c);
+    
+    let maxEnergy = 0;
+    for(let x=0; x<grid.width; x++){
+        console.log(`Down from x = ${x}..`)
+        maxEnergy = Math.max(maxEnergy, energize({ x, y: 0, direction: 1 }, grid));
+        console.log(`Up from x = ${x}..`)
+        maxEnergy = Math.max(maxEnergy, energize({ x, y: grid.maxY, direction: 3 }, grid));
+    }
+
+    for(let y=0; y<grid.height; y++){
+        console.log(`Right from y = ${y}..`)
+        maxEnergy = Math.max(maxEnergy, energize({ x:0, y, direction: 0 }, grid));
+        console.log(`Left from y = ${y}..`)
+        maxEnergy = Math.max(maxEnergy, energize({ x: grid.maxX, y, direction: 2 }, grid));
+    }
+
+    return `${maxEnergy}`;
+}
+
+function energize(tail: { x: number; y: number; direction: number; }, grid: Grid<string>) : number{
+    const energyLevels = new Grid<number>(grid.width, grid.height);
+    for(let x=0; x<energyLevels.width; x++){
+        for(let y=0; y<energyLevels.height; y++){
+            energyLevels.setCell(x,y,0);
+        }
+    }
+    const tails = [tail];  // 0 is left to right, 1 top to bottom, 2, right to left, 3, bottom to top
+    let steps = 0;
+    do {
+        //if (steps % 1000) console.log(`${steps} : TAILS : ${JSON.stringify(tails)}`);
+
+        for (let i = 0; i < tails.length; i++) {
+
+            const tail = tails[i];
+            if (tail.x < 0 || tail.x > grid.maxX || tail.y < 0 || tail.y > grid.maxY) {
+                //console.log(`${steps} :   hit wall at ${JSON.stringify(tail)} - removing tail[${i}] : `)
+                tails.splice(i, 1);
+                continue;
+            }
+            //console.log(`${steps} : -- TAILS[${i}] : ${JSON.stringify(tail)}`);
+            const cell = grid.getCell(tail.x, tail.y)!;
+
+            //console.log(`${steps} : -- TAILS[${i}] : ${JSON.stringify(tail)} : cell : ${cell}`);
+            energyLevels.setCell(tail.x, tail.y, energyLevels.getCell(tail.x, tail.y)! + 1);
+
+            switch (cell) {
+                case '.': {
+                    switch (tail.direction) { // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+                        case 0: {
+                            goRight(tail);
+                            break;
+                        }
+                        case 1: {
+                            goDown(tail);
+                            break;
+                        }
+                        case 2: {
+                            goLeft(tail);
+                            break;
+                        }
+                        case 3: {
+                            goUp(tail);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case '/': {
+                    switch (tail.direction) { // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+                        case 0: {
+                            goUp(tail);
+                            break;
+                        }
+                        case 1: {
+                            goLeft(tail);
+                            break;
+                        }
+                        case 2: {
+                            goDown(tail);
+                            break;
+                        }
+                        case 3: {
+                            goRight(tail);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case '\\': {
+                    switch (tail.direction) { // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+                        case 0: {
+                            goDown(tail);
+                            break;
+                        }
+                        case 1: {
+                            goRight(tail);
+                            break;
+                        }
+                        case 2: {
+                            goUp(tail);
+                            break;
+                        }
+                        case 3: {
+                            goLeft(tail);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case '-': {
+                    switch (tail.direction) { // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+                        case 0: {
+                            goRight(tail);
+                            break;
+                        }
+                        case 2: {
+                            goLeft(tail);
+                            break;
+                        }
+                        case 1:
+                        case 3: {
+                            // split the beam
+                            const splitLocation = { x: tail.x, y: tail.y, direction: tail.direction };
+                            tails.push(splitLocation);
+
+                            goRight(tail);
+                            goLeft(splitLocation);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case '|': {
+                    switch (tail.direction) { // 0 is left to right, 1 top to bottom, 2 right to left, 3 bottom to top
+                        case 1: {
+                            goDown(tail);
+                            break;
+                        }
+                        case 3: {
+                            goUp(tail);
+                            break;
+                        }
+                        case 0:
+                        case 2: {
+                            // split the beam
+                            const splitLocation = { x: tail.x, y: tail.y, direction: tail.direction };
+                            tails.push(splitLocation);
+
+                            goDown(tail);
+                            goUp(splitLocation);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (let j = tails.length - 1; j >= 0; j--) {
+            let tail = tails[j];
+            if (tail.x < 0 || tail.x > grid.maxX || tail.y < 0 || tail.y > grid.maxY) {
+                //console.log(`${steps} :   hit wall at ${JSON.stringify(tail)} - removing tail[${j}] : `)
+                tails.splice(j, 1);
+            } else {
+                let energy = energyLevels.getCell(tail.x, tail.y)!;
+                if (energy > 1000) {
+                    // console.log(`${steps} :   spike at ${JSON.stringify(tail)} - removing tail[${j}] : `)
+                    tails.splice(j, 1);
+                }
+            }
+        }
+
+        steps++;
+    } while (tails.length > 0);
+
+
+    //energyLevels.logToConsole(e => e! > 0 ? '#' : '.');
+    let energised = 0;
+    for (let x = 0; x < energyLevels.width; x++) {
+        for (let y = 0; y < energyLevels.height; y++) {
+            energised += energyLevels.getCell(x, y)! > 0 ? 1 : 0;
+        }
+    }
+
+
+    return energised;
 }
