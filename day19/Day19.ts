317aaf47abb579eb94d89f4d1172c4b051074d7b
Franck Schmidlin <franck.schmidlin@theverygroup.com>
2023-12-19 09:09:18 +0000

    Day 19 - part 1 - raw

--- day19/Day19.ts
+++ day19/Day19.ts
@@ -17,127 +17,106 @@ part2.inputFile = 'input.txt';
 /////////////////////////////
 // ACTUAL CODE - Part ONE  //
 /////////////////////////////
-type Rule ={
-    xG : number,
-    xL : number,
-    xDestination : string,
-    mG : number,
-    mL : number,
-    mDestination : string,
-    aG : number,
-    aL : number,
-    aDestination : string,
-    sG : number,
-    sL : number,
-    sDestination : string,
-    defaultDestination : string
+type Rule = {
+    category: string,
+    testForGreaterThan: boolean,
+    test: number,
+    destination: string
 }
-// type Workflow = {
-//     id: string,
-//     rule: Rule,
-// }
+type Workflow = {
+    id: string,
+    rules: Rule[],
+    defaultDestination: string
+}
 type Rating = {
-    x:number,
-    m:number,
-    a:number,
-    s:number
+    x: number,
+    m: number,
+    a: number,
+    s: number
 }
 function part1Implementation(entries: string[]) {
-    const workflows = new Map<string, Rule>();
+    const workflows = new Map<string, Workflow>();
     const ratings = [] as Rating[];
     let readingRatings = false;
-    for(let i=0; i<entries.length; i++){
+    for (let i = 0; i < entries.length; i++) {
         console.log(`parsing ${entries[i]}...`)
-        if(entries[i].length === 0) {
+        if (entries[i].length === 0) {
             readingRatings = true;
             continue;
         }
 
-        if(readingRatings){
-            
-            const g= entries[i].match(/\{x=(?<x>.*),m=(?<a>.*),a=(?<m>.*),s=(?<s>.*)\}/)!.groups!;
-            ratings.push({x:parseInt(g.x), m:parseInt(g.m), a:parseInt(g.a), s:parseInt(g.s)});
+        if (readingRatings) {
+
+            const g = entries[i].match(/\{x=(?<x>.*),m=(?<m>.*),a=(?<a>.*),s=(?<s>.*)\}/)!.groups!;
+            ratings.push({ x: parseInt(g.x), m: parseInt(g.m), a: parseInt(g.a), s: parseInt(g.s) });
             continue;
         }
 
         const g = entries[i].match(/(?<id>.*)\{(?<rules>.*)\}/)!.groups!;
         const rulesParts = g.rules.split(',');
-        const rule = {} as Rule;
-        rule.defaultDestination = rulesParts.pop()!;
-        rule.xG = Number.POSITIVE_INFINITY,
-        rule.xL = Number.NEGATIVE_INFINITY;
-        rule.xDestination = 'n/a';
-        rule.mG = Number.POSITIVE_INFINITY,
-        rule.mL = Number.NEGATIVE_INFINITY;
-        rule.mDestination = 'n/a';
-        rule.aG = Number.POSITIVE_INFINITY,
-        rule.aL = Number.NEGATIVE_INFINITY;
-        rule.aDestination = 'n/a';
-        rule.sG = Number.POSITIVE_INFINITY,
-        rule.sL = Number.NEGATIVE_INFINITY;
-        rule.sDestination = 'n/a';
+        const workflow = {} as Workflow;
+        workflow.defaultDestination = rulesParts.pop()!;
+        workflow.rules = [] as Rule[];
+        workflow.id = g.id;
 
-        rulesParts.forEach(part=> {
-           const r = part.match(/(?<idAndSign>.{2})(?<test>\d*):(?<destination>.*)/)!.groups!
-           switch(r.idAndSign){
-            case 'x>' : {
-                rule.xG = parseInt(r.test);
-                rule.xL = Number.NEGATIVE_INFINITY;
-                rule.xDestination = r.destination;
-                break
-            }
-            case 'x<' : {
-                rule.xL = parseInt(r.test);
-                rule.xG = Number.POSITIVE_INFINITY;
-                rule.xDestination = r.destination;
-                break
-            }
-            case 'm>' : {
-                rule.mG = parseInt(r.test);
-                rule.mL = Number.NEGATIVE_INFINITY;
-                rule.mDestination = r.destination;
-                break
-            }
-            case 'x<' : {
-                rule.mL = parseInt(r.test);
-                rule.mG = Number.POSITIVE_INFINITY;
-                rule.mDestination = r.destination;
-                break
-            }
-            case 'a>' : {
-                rule.aG = parseInt(r.test);
-                rule.aL = Number.NEGATIVE_INFINITY;
-                rule.aDestination = r.destination;
-                break
-            }
-            case 'x<' : {
-                rule.aL = parseInt(r.test);
-                rule.aG = Number.POSITIVE_INFINITY;
-                rule.aDestination = r.destination;
-                break
-            }
-            case 's>' : {
-                rule.sG = parseInt(r.test);
-                rule.sL = Number.NEGATIVE_INFINITY;
-                rule.sDestination = r.destination;
-                break
-            }
-            case 'x<' : {
-                rule.sL = parseInt(r.test);
-                rule.sG = Number.POSITIVE_INFINITY;
-                rule.sDestination = r.destination;
-                break
-            }
-        }
-        //console.log(`${g.id} : rule = ${JSON.stringify(rule, (_,v)=>v==Infinity?'∞':v)}`)
+
+        rulesParts.forEach(part => {
+            const r = part.match(/(?<category>.)(?<sign>.)(?<test>\d*):(?<destination>.*)/)!.groups!
+            workflow.rules.push({
+                category: r.category,
+                testForGreaterThan: (r.sign === '>'),
+                test: parseInt(r.test),
+                destination: r.destination
+            })
+            //console.log(`${g.id} : rule = ${JSON.stringify(rule, (_,v)=>v==Infinity?'∞':v)}`)
         });
 
-        workflows.set(g.id, rule);
+        workflows.set(g.id, workflow);
     }
 
-    workflows.forEach((rule, id) => console.log(`${id} : ${JSON.stringify(rule,(_,v)=>Math.abs(v)==Infinity?'∞':v)}`))
+    workflows.forEach((workflow, id) => {
+        console.log(`${id} : default destination = ${workflow.defaultDestination}, rules :`);
+        workflow.rules.forEach(r => console.log(`${JSON.stringify(r)}`));
+    });
 
-    let solution = '???'
+    const accepted = [] as Rating[];
+    ratings.forEach(rating => {
+
+        let destination = 'in';
+        let done = false;
+        while (!done) {
+            let workflow = workflows.get(destination)!;
+            let matchesRule = false;
+            for (let i = 0; i < workflow.rules.length; i++) {
+                const rule = workflow.rules[i];
+                const category = rating[rule.category as keyof Rating];
+                matchesRule = rule.testForGreaterThan ? (category > rule.test) : (category < rule.test);
+                if (matchesRule) {
+                    destination = rule.destination;
+                    break;
+                }
+            }
+            if(!matchesRule) destination = workflow.defaultDestination;
+            if (destination == 'A') {
+                accepted.push(rating);
+            }
+            console.log(`Part: ${JSON.stringify(rating)} : workflow ${workflow.id} destination -> ${destination}`);
+            // some weird TS shite here
+            // getting TS2367: This comparison appears to be unintentional because the types '"A"' and '"R"' have no overlap.
+            // if I simply try one = destination === 'A' || destination === 'R';
+            done = destination === 'A';
+            done = done || destination === 'R';
+        }
+    });
+
+    let solution = 0;
+    console.log(`${accepted.length} parts were accepted:`)
+    accepted.forEach(a => {
+        const totalRating = a.x + a.m + a.a + a.s;
+        console.log(`${JSON.stringify(a)}: ${totalRating}`);
+        solution += totalRating;
+    });
+
     return `${solution}`;
 }
 
